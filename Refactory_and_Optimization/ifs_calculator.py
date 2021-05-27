945590612d6b8212ac20f453515df07689fa5e4d
Ali@dev <mauricioali026@gmail.com>
2021-05-27 15:37:27 -0500

    isf_calculator.py

---
+++ Refactory_and_Optimization/ifs_calculator.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def graphic(currencies:int):
+    
+    if_cases=[]
+    xlabel=[]
+    
+    for index,coins in enumerate(range(1,currencies+1)):
+        
+        case=coins**2
+        if_cases.append(case)
+        xlabel.append(coins)        
+        print(index,if_cases[-1],"combinations")
+    
+    fig,ax = plt.subplots()
+    ax.plot(xlabel,if_cases)
+    ax.set(xlabel='currencies', ylabel='if_cases',title='maintainability')
+    ax.grid()
+    plt.show()
+
+                
+graphic(183)
+
+
